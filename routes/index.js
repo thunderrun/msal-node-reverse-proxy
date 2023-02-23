60ad491db01a223df355cf3b38868e366ff7f521
Zou Yun <yun.zou@gonvvama.com>
2023-02-23 16:29:43 +0800

    update .env

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,10 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function (req, res, next) {
-    res.redirect(process.env.PROXY_APP_HOMEPAGE)
+    if (process.env.PROXY_APP_HOMEPAGE !== '/') {
+        return res.redirect(process.env.PROXY_APP_HOMEPAGE);
+    }
+    next();
 });
 
 module.exports = router;
