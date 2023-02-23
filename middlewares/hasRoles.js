90aa03f03f03cb5bb4d9eed3c40a96052691c8f0
Zou Yun <yun.zou@gonvvama.com>
2023-02-23 10:40:36 +0800

    add regexp

--- middlewares/hasRoles.js
+++ middlewares/hasRoles.js
@@ -6,8 +6,9 @@ const hasRoles = (req, res, next) => {
         if (!userRoles) {
             return res.send("You don't have any roles. Ask your administrator to assign the roles to your Microsoft account.");
         }
-        Object.entries(pathRoleSettings).forEach((path, roles) => {
-            if (req.path === path) {
+        Object.entries(pathRoleSettings).forEach(([path, roles]) => {
+            const regexp = new RegExp(path)
+            if (req.originalUrl.match(regexp)) {
                 const intersection = userRoles.filter(value => roles.includes(value));
                 if (!intersection.length) {
                     return res.send("You don't have permission of the current path. Ask your administrator to assign the correct roles to your Microsoft account.");
--- pathRoleSettings.js
+++ pathRoleSettings.js
@@ -1,3 +1,6 @@
+// using new RegExp() for matching URL with a pattern
+
 module.exports = {
-    '/GonvvamaReport/ureport/designer?_u=file:cs_pallet_sort.ureport.xml': ['Admin']
+    '/GonvvamaReport/ureport/designer': ['Admin'],
+    '/GonvvamaReport/ureport/designer\\?_u=file:[^ ]*test.ureport.xml': ['CN52']
 }
