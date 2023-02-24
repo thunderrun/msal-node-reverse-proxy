796637c6655e7599e0e579a65edf9b1afe06c7e4
Zou Yun <yun.zou@gonvvama.com>
2023-02-24 16:34:54 +0800

    update comment

--- pathRoleSettings.example.js
+++ pathRoleSettings.example.js
@@ -1,6 +1,6 @@
 // using RegExp to match URL
 
 module.exports = {
-    '/path-required-roles': ['Role1', 'Role2'],
+    '/path-required-roles': ['Role1', 'Role2'], // users with Role1 or Role2 will have access to path /path-required-roles
     '/path\\?file=[^ ]*test.ext': ['Role1'], // match query
 }
