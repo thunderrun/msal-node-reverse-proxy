5c1d0f739f7e18382be77ba8c23d4c21be1981ab
Zou Yun <yun.zou@gonvvama.com>
2023-02-24 11:25:58 +0800

    untrack gitignored files

--- pathRoleSettings.js
+++
@@ -1,6 +0,0 @@
-// using new RegExp() for matching URL with a pattern
-
-module.exports = {
-    '/GonvvamaReport/ureport/designer': ['Admin'],
-    '/GonvvamaReport/ureport/designer\\?_u=file:[^ ]*test.ureport.xml': ['CN52']
-}
