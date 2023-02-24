590edb3979ddb39f7e75e5a440691d9daf82cb66
Zou Yun <yun.zou@gonvvama.com>
2023-02-24 11:26:38 +0800

    update git ignore

---
+++ pathRoleSettings.js
@@ -0,0 +1,6 @@
+// using new RegExp() for matching URL with a pattern
+
+module.exports = {
+    '/GonvvamaReport/ureport/designer': ['Admin'],
+    '/GonvvamaReport/ureport/designer\\?_u=file:[^ ]*test.ureport.xml': ['CN52']
+}
