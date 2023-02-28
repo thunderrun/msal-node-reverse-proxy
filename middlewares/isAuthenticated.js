667418f7efefb8c6c28b9f161da360458f2629e8
Zou Yun <yun.zou@gonvvama.com>
2023-02-28 14:49:53 +0800

    update isAuthenticated redirect

--- middlewares/isAuthenticated.js
+++ middlewares/isAuthenticated.js
@@ -1,6 +1,6 @@
 const isAuthenticated = (req, res, next) => {
     if (!req.session.isAuthenticated) {
-        return res.redirect(`/auth/signin?originalUrl=${req.originalUrl}`); // redirect to sign-in route
+        return res.redirect(`${process.env.PROXY_PATH}/auth/signin?originalUrl=${req.originalUrl}`); // redirect to sign-in route
     }
     next();
 };
